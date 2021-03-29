ba9ab062a6853fee59ec049454fdabd493b23b56
Karsten Bolding <karsten@bolding-bruggeman.com>
2021-03-29 12:41:02 +0200

    obs: use intra-communicators, communicate with both model and filter

--- src/eat_observations.F90
+++ src/eat_observations.F90
@@ -8,11 +8,15 @@ program eat_observations
 
    USE, INTRINSIC :: ISO_FORTRAN_ENV
    use mpi
-   use mpi_config
+   use eat_config
    IMPLICIT NONE
 
    integer :: ierr
    character(32), allocatable  :: obs_times(:)
+   logical :: have_model=.true.
+   logical :: have_filter=.true.
+   integer :: nmodel=-1
+!KB   integer :: nfilter=-1
 !-----------------------------------------------------------------------
 
    call init_observations()
@@ -35,7 +39,28 @@ subroutine init_observations()
    character(len=32) :: buf
    integer :: i,n
 !-----------------------------------------------------------------------
-   call init_mpi_config(color_obs)
+   call init_eat_config(color_obs)
+
+   if (size_obs_comm == size_obs_model_comm) then
+      write(error_unit,*) "obs(no model executable present)"
+      have_model=.false.
+      EAT_COMM_obs_model=MPI_COMM_NULL
+   else
+      call MPI_COMM_RANK(EAT_COMM_obs_model,rank_obs_model_comm,ierr)
+      call MPI_COMM_SIZE(EAT_COMM_obs_model,nmodel,ierr)
+      nmodel=nmodel-1 !KB - need size of obs comm
+   end if
+
+   if (size_obs_comm == size_obs_filter_comm) then
+      write(error_unit,*) "obs(no filter executable present)"
+      have_filter=.false.
+      EAT_COMM_obs_filter=MPI_COMM_NULL
+   else
+      call MPI_COMM_RANK(EAT_COMM_obs_filter,rank_obs_filter_comm,ierr)
+   end if
+
+   write(error_unit,'(A,3I5)') ' obs(ranks: O-OM-OF):    ',rank_obs_comm,rank_obs_model_comm,rank_obs_filter_comm
+   write(error_unit,'(A,3I5)') ' obs(sizes: O-OM-OF):    ',size_obs_comm,size_obs_model_comm,size_obs_filter_comm
 
    open(newunit=unit,file="obs_times.dat",status='old',action='read',iostat=ios)
    if (ios /= 0) stop 'init_obs(): unable to open obs_times.dat for reading'
@@ -62,28 +87,48 @@ subroutine do_observations()
 
    ! Local variables
    integer :: stat(MPI_STATUS_SIZE)
-   integer :: n,nobs
+   integer :: m,n,nobs
    real(real64), allocatable :: obs(:)
    character(len=32) :: timestr,halt="0000-00-00 00:00:00"
    integer :: request
 !-----------------------------------------------------------------------
    do n=1,size(obs_times)
-      call MPI_SEND(obs_times(n),19,MPI_CHARACTER,0,1,MPI_COMM_obs,ierr)
-      nobs=10000*n
-      call MPI_SEND(nobs,1,MPI_INTEGER,0,1,MPI_COMM_obs,ierr)
-      if (nobs > 0) then
-         if (.not. allocated(obs)) allocate(obs(nobs))
-         if (allocated(obs) .and. nobs > size(obs)) then
-             deallocate(obs)
-             allocate(obs(nobs))
+      if (have_model) then
+         do m=1,nmodel
+            call MPI_SEND(obs_times(n),19,MPI_CHARACTER,m,m,EAT_COMM_obs_model,ierr)
+         end do
+      else
+         write(error_unit,*) 'obs(-> time)  ',trim(obs_times(n))
+      end if
+      if (have_filter) then
+         nobs=10000*n
+         write(error_unit,'(A,I6)') ' obs(-> nobs)    ',nobs
+         call MPI_SEND(nobs,1,MPI_INTEGER,1,1,EAT_COMM_obs_filter,ierr)
+         if (nobs > 0) then
+            if (.not. allocated(obs)) allocate(obs(nobs))
+            if (allocated(obs) .and. nobs > size(obs)) then
+                deallocate(obs)
+                allocate(obs(nobs))
+            end if
+            CALL RANDOM_NUMBER(obs)
+            call MPI_ISEND(obs,nobs,MPI_DOUBLE,1,1,EAT_COMM_obs_filter,request,ierr)
+            call sleep(1)
+            call MPI_WAIT(request,stat,ierr)
          end if
-         CALL RANDOM_NUMBER(obs)
-         call MPI_ISEND(obs,nobs,MPI_DOUBLE,0,1,MPI_COMM_obs,request,ierr)
-         call sleep(1)
-         call MPI_WAIT(request,stat,ierr)
       end if
    end do
-   call MPI_SEND(halt,16,MPI_CHARACTER,0,1,MPI_COMM_obs,ierr)
+   if (have_model) then
+      do m=1,nmodel
+         call MPI_SEND(halt,19,MPI_CHARACTER,m,m,EAT_COMM_obs_model,ierr) !!!! nmodel
+      end do
+   else
+      write(error_unit,*) 'obs(no more obs) -> halting'
+   end if
+   if (have_filter) then
+      nobs=-1
+      write(error_unit,'(A,I6)') ' obs(-> nobs)    ',nobs
+      call MPI_SEND(nobs,1,MPI_INTEGER,1,1,EAT_COMM_obs_filter,ierr)
+   end if
 end subroutine do_observations
 
 !-----------------------------------------------------------------------
