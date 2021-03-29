c10cd398acf1d02ab66f8d35eb3ecc6d4a98d1c3
Karsten Bolding <karsten@bolding-bruggeman.com>
2021-03-29 12:43:11 +0200

    pdaf: rudimentary - receive obs - not receiving model states yet

--- src/eat_pdaf.F90
+++ src/eat_pdaf.F90
@@ -7,10 +7,12 @@ program eat_pdaf
 
    USE, INTRINSIC :: ISO_FORTRAN_ENV
    use mpi
-   use mpi_config
+   use eat_config
    IMPLICIT NONE
 
    integer :: ierr
+   logical :: have_obs=.true.
+   logical :: have_model=.true.
 !-----------------------------------------------------------------------
 
    call eat_init_pdaf()
@@ -29,7 +31,26 @@ subroutine eat_init_pdaf()
 
    ! Local variables
 !-----------------------------------------------------------------------
-   call init_mpi_config(color_filter)
+   call init_eat_config(color_filter)
+
+   if (size_filter_comm == size_obs_filter_comm) then
+      write(error_unit,*) "filter(no observation executable present)"
+      have_obs=.false.
+      EAT_COMM_obs_filter=MPI_COMM_NULL
+   else
+      call MPI_COMM_RANK(EAT_COMM_obs_filter,rank_obs_filter_comm,ierr)
+   end if
+
+   if (size_filter_comm == size_model_filter_comm) then
+      write(error_unit,*) "filter(no model executable present)"
+      have_model=.false.
+      EAT_COMM_model_filter=MPI_COMM_NULL
+   else
+      call MPI_COMM_RANK(EAT_COMM_model_filter,rank_model_filter_comm,ierr)
+   end if
+
+   write(error_unit,'(A,3I5)') ' filter(ranks: F-OF-MF): ',rank_filter_comm,rank_obs_filter_comm,rank_model_filter_comm
+   write(error_unit,'(A,3I5)') ' filter(sizes: F-OF-MF): ',size_filter_comm,size_obs_filter_comm,size_model_filter_comm
 end subroutine eat_init_pdaf
 
 !-----------------------------------------------------------------------
@@ -50,7 +71,8 @@ subroutine eat_do_pdaf()
    integer :: filter_stats(MPI_STATUS_SIZE,2)
 !-----------------------------------------------------------------------
    do
-      call MPI_RECV(recv_signal,4,MPI_INTEGER,0,1,MPI_COMM_filter,stat,ierr)
+#if 0
+      call MPI_RECV(recv_signal,4,MPI_INTEGER,0,1,EAT_COMM_filter,stat,ierr)
       if (ierr /= MPI_SUCCESS) then
          call MPI_ABORT(MPI_COMM_WORLD,2,ierr)
       end if
@@ -61,22 +83,28 @@ subroutine eat_do_pdaf()
          state_size=recv_signal(2)
          nensemble=recv_signal(3)
          nobs=recv_signal(4)
-#if 1
          if (.not. allocated(states)) allocate(states(state_size,nensemble))
          if (state_size > size(states,1) .or. nensemble > size(states,2)) then
             deallocate(states)
             allocate(states(state_size,nensemble))
          end if
-         call MPI_IRECV(states,nensemble*state_size,MPI_DOUBLE,0,1,MPI_COMM_filter,filter_reqs(1),ierr)
-         if (.not. allocated(obs)) allocate(obs(nobs))
-         if (nobs > size(obs)) then
-            deallocate(obs)
-            allocate(obs(nobs))
-         end if
-         call MPI_IRECV(obs,nobs,MPI_DOUBLE,0,1,MPI_COMM_filter,filter_reqs(2),ierr)
-         call MPI_WAITALL(2,filter_reqs,filter_stats,ierr)
-         write(error_unit,*) 'filter - recv obs ',sum(obs)/nobs
+         call MPI_IRECV(states,nensemble*state_size,MPI_DOUBLE,0,1,EAT_COMM_filter,filter_reqs(1),ierr)
 #endif
+      if (have_obs) then
+         call MPI_RECV(nobs,1,MPI_INTEGER,0,1,EAT_COMM_obs_filter,stat,ierr)
+         write(error_unit,'(A,I6)') ' filter(<- nobs) ',nobs
+         if (.not. allocated(obs)) allocate(obs(nobs))
+         if (nobs > 0) then
+            if (nobs > size(obs)) then
+               deallocate(obs)
+               allocate(obs(nobs))
+            end if
+            call MPI_IRECV(obs,nobs,MPI_DOUBLE,0,1,EAT_COMM_obs_filter,filter_reqs(1),ierr)
+            call MPI_WAITALL(1,filter_reqs,filter_stats,ierr)
+            write(error_unit,*) 'filter(<- obs) ',sum(obs)/nobs
+         else
+            exit
+         end if
       end if
    end do
 end subroutine eat_do_pdaf
