fae28ada26d0b58ade07ae1e84a4d915ab3aedd5
Karsten Bolding <karsten@bolding-bruggeman.com>
2021-07-05 17:42:25 +0200

    plot_fileds: initial support for plotting differences between fields

--- tests/2d/plot_fields.py
+++ tests/2d/plot_fields.py
@@ -5,33 +5,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-def plot_field(infiles,noshow=False,save=False):
-   for f in infiles:
-      print(f.name)
-      data = np.loadtxt(f)
-      plt.figure()
-      plt.imshow(data, origin='lower', interpolation='none')
-      plt.title(f.name)
-      if save:
-         plt.savefig(f.name.rsplit('.', 1)[0] + '.png')
-
-def animate_fields(infiles,save=False,interval=100,title=''):
+def read_data(filelist):
    data = []
-   for f in infiles:
+   for f in filelist:
       print(f.name)
       data.append(np.loadtxt(f))
    data = np.dstack(tuple(data))
-   print(data.shape)
+   return data
 
+def plot_field(data,noshow=False,save=False,title='',vmin=-1,mmax=1):
+   for n in range(data.shape[2]):
+      plt.figure()
+      m = data[:,:,n].mean()
+      plt.imshow(data[:,:,n], origin='lower', interpolation='none', vmin=vmin, vmax=vmax)
+      plt.colorbar()
+      plt.title('{0} ({1:03} - {2:6.4f})'.format(title,n,m))
+      if save:
+         plt.savefig(f.name.rsplit('.', 1)[0] + '.png')
+
+def animate_fields(data,save=False,interval=100,title='',vmin=-1,vmax=1):
    def animate(i):
-      ax.set_title('{0} ({1:03})'.format(title,i))
+      m = data.mean()
+      ax.set_title('{0} ({1:03} - {2:6.4f})'.format(title,i,m))
       cax.set_array(data[:,:,i].flatten())
 
    fig, ax = plt.subplots(figsize=(6, 3))
    ax.set(xlim=(-1, 37), ylim=(-1, 19))
-   cax = ax.pcolormesh(data[:,:,0], cmap='Blues')
+   cax = ax.pcolormesh(data[:,:,0], cmap='Blues', vmin=vmin, vmax=vmax)
+#KB   cax = ax.imshow(data[:,:,0], cmap='Blues')
    fig.colorbar(cax)
-   anim = FuncAnimation(fig, animate, interval=interval, frames=len(infiles)-1)
+   anim = FuncAnimation(fig, animate, interval=interval, frames=data.shape[2])
 #   plt.draw()
    if save:
       anim.save('eat_2d.mp4', fps=3)
@@ -43,16 +46,35 @@ if __name__ == "__main__":
 
    parser = argparse.ArgumentParser(description='Plot/animate 2D field(s).')
    parser.add_argument('infiles', type=argparse.FileType('r'), nargs='*')
+   parser.add_argument('--diff', action='store_true', help=' take difference between files')
    parser.add_argument('--noshow', action='store_true', help=' do not show on screen')
    parser.add_argument('--save', action='store_true', help='save png/mp4 file(s)')
    parser.add_argument('--animate', action='store_true', help='animate input files')
    parser.add_argument('--interval', default=100, help='interval (ms) between updates')
    parser.add_argument('--title', default='', help='animation title')
+   parser.add_argument('--vmin', default='-1', help='minimum')
+   parser.add_argument('--vmax', default='1', help='minimum')
    args = parser.parse_args()
 
+   if not args.diff:
+      data=read_data(args.infiles)
+   else:
+      N=len(args.infiles)//2
+      #KB - maybe better to use filter instead
+      data=read_data(args.infiles[N::])
+#KB      print('AAA')
+#KB      print(data[0,0,5])
+      data1=read_data(args.infiles[:N:])
+      data=data-data1
+#KB      print(data1[0,0,5])
+#KB      print(data[0,0,5])
+#KB      print('BBB')
+      
+   print(data.shape)
+
    if not args.animate:
-       plot_field(args.infiles, save=args.save)
+       plot_field(data,save=args.save,title=args.title,vmin=args.vmin,vmax=args.vmax)
        if not args.noshow:
           plt.show()
    else:
-       animate_fields(args.infiles,save=args.save,interval=args.interval,title=args.title)
+       animate_fields(data,save=args.save,interval=args.interval,title=args.title,vmin=args.vmin,vmax=args.vmax)
