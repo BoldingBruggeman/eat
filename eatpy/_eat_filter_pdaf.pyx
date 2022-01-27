204a006977ab4e6dce8f5185144b12daf8e515b9
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2022-01-27 11:07:16 +0000

    moved result to return value of cvt callbacks in python

--- eatpy/_eat_filter_pdaf.pyx
+++ eatpy/_eat_filter_pdaf.pyx
@@ -40,16 +40,24 @@ cdef void ccvt_callback(int cb_type, int iter, int dim_p, int dim_ens, int dim_c
     if cvt_handler_ is not None:
         if cb_type == 1:
             v_p_.flags.writeable = False
-            cvt_handler_.cvt(iter, v_p_, Vv_p_)
+            result = cvt_handler_.cvt(iter, v_p_)
+            assert numpy.shape(result) == Vv_p_.shape, 'Array returned by cvt should have shape %s, but it has shape %s.' % (Vv_p_.shape, numpy.shape(result))
+            Vv_p_[:] = result
         elif cb_type == 2:
             Vv_p_.flags.writeable = False
-            cvt_handler_.cvt_adj(iter, Vv_p_, v_p_)
+            result = cvt_handler_.cvt_adj(iter, Vv_p_)
+            assert numpy.shape(result) == v_p_.shape, 'Array returned by cvt_adj should have shape %s, but it has shape %s.' % (v_p_.shape, numpy.shape(result))
+            v_p_[:] = result
         elif cb_type == 3:
             assert ens_p_ is not None
             v_p_.flags.writeable = False
-            cvt_handler_.cvt_ens(iter, ens_p_, v_p_, Vv_p_)
+            result = cvt_handler_.cvt_ens(iter, ens_p_, v_p_)
+            assert numpy.shape(result) == Vv_p_.shape, 'Array returned by cvt_ens should have shape %s, but it has shape %s.' % (Vv_p_.shape, numpy.shape(result))
+            Vv_p_[:] = result
         elif cb_type == 4:
             assert ens_p_ is not None
             Vv_p_.flags.writeable = False
-            cvt_handler_.cvt_adj_ens(iter, ens_p_, Vv_p_, v_p_)
+            result = cvt_handler_.cvt_adj_ens(iter, ens_p_, Vv_p_)
+            assert numpy.shape(result) == v_p_.shape, 'Array returned by cvt_adj_ens should have shape %s, but it has shape %s.' % (v_p_.shape, numpy.shape(result))
+            v_p_[:] = result
 
