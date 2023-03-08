0ef9e0583344b76b023025a7017921f44161b3c5
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2023-03-08 10:56:51 +0000

    transform.Log: also transform observation sd; added log10 argument

--- eatpy/plugins/transform.py
+++ eatpy/plugins/transform.py
@@ -1,4 +1,4 @@
-from typing import Any, List, MutableMapping, Optional
+from typing import Any, List, MutableMapping
 import datetime
 
 import numpy as np
@@ -8,12 +8,19 @@ from .. import shared
 
 class Log(shared.Plugin):
     def __init__(
-        self, *variable_names, transform_obs: bool = True, minimum: float = -np.inf
+        self,
+        *variable_names,
+        transform_obs: bool = True,
+        minimum: float = -np.inf,
+        log10: bool = True
     ):
         self.variable_names = frozenset(variable_names)
         self.variable_metadata: List[Any] = []
         self.transform_obs = transform_obs
         self.minimum = minimum
+        self.log10 = log10
+        self.forward = np.log10 if self.log10 else np.log
+        self.backward = (lambda x: 10.0 ** x) if self.log10 else np.exp
 
     def initialize(self, variables: MutableMapping[str, Any], *args, **kwargs):
         for name in self.variable_names:
@@ -25,15 +32,29 @@ class Log(shared.Plugin):
         state: np.ndarray,
         iobs: np.ndarray,
         obs: np.ndarray,
+        obs_sds: np.ndarray,
         *args,
         **kwargs
     ):
         for metadata in self.variable_metadata:
             affected_obs = (iobs >= metadata["start"]) & (iobs < metadata["stop"])
             if self.transform_obs and affected_obs.any():
-                obs[affected_obs] = np.log10(np.maximum(obs[affected_obs], self.minimum))
-            metadata["data"][...] = np.log10(np.maximum(metadata["data"], self.minimum))
+                # Transform mean and sd of observations,
+                # assuming their distribution is log-normal
+                mean = np.maximum(obs[affected_obs], self.minimum)
+                sd = obs_sds[affected_obs]
+                sigma2 = np.log((sd / mean) ** 2 + 1.0)
+                mu = np.log(mean) - 0.5 * sigma2
+                sigma = np.sqrt(sigma2)
+                if self.log10:
+                    mu /= np.log(10.0)
+                    sigma /= np.log(10.0)
+                obs[affected_obs] = mu
+                obs_sds[affected_obs] = sigma
+            metadata["data"][...] = self.forward(
+                np.maximum(metadata["data"], self.minimum)
+            )
 
     def after_analysis(self, *args, **kwargs):
         for metadata in self.variable_metadata:
-            metadata["data"][...] = 10.0 ** metadata["data"]
+            metadata["data"][...] = self.backward(metadata["data"])
