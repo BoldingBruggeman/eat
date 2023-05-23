1cc61768d74fcb518d1a3a159c0feb8ae014ef30
Karsten Bolding <karsten@bolding-bruggeman.com>
2023-05-23 13:34:11 +0200

    fixed building with conda build

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -5,9 +5,9 @@ cd build
 
 external_model_dir=$RECIPE_DIR/../extern
 
-default_institutes="akvaplan;au;bb;csiro;examples;gotm;iow;jrc;msi;niva;pclake;pml;selma;su;uhh"
+default_institutes="akvaplan;au;bb;csiro;examples;gotm;iow;jrc;msi;nersc;niva;pclake;pml;selma;su;uhh"
 external_institutes="ersem;ogs;pisces"
-active_institutes="-DFABM_INSTITUTES=$default_institutes;$external_institutes"
+fabm_institutes="-DFABM_INSTITUTES=$default_institutes;$external_institutes"
 
 model_dirs="-DFABM_ERSEM_BASE=$external_model_dir/ersem -DFABM_OGS_BASE=$external_model_dir/ogs -DFABM_PISCES_BASE=$external_model_dir/pisces"
 
@@ -18,8 +18,7 @@ else
   CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
 fi
 
-echo $RECIPE_DIR/.. -DPython3_EXECUTABLE="$PYTHON" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX ${CMAKE_PLATFORM_FLAGS[@]} $active_institutes $model_dirs
-cmake $RECIPE_DIR/.. -DPython3_EXECUTABLE="$PYTHON" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX ${CMAKE_PLATFORM_FLAGS[@]} $active_institutes $model_dirs
+cmake $RECIPE_DIR/.. -DPython3_EXECUTABLE="$PYTHON" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX ${CMAKE_PLATFORM_FLAGS[@]} $fabm_institutes $model_dirs
 cmake --build . --config Release --parallel $CPU_COUNT --target install
 cd ..
 rm -r build
