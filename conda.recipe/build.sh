3b3d47d6a2e1cc4bb2f88efed1e3a6a02f05253f
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2023-11-07 14:03:42 +0000

    use FABM_EXTRA_INSTITUTES

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -4,11 +4,7 @@ mkdir build
 cd build
 
 external_model_dir=$RECIPE_DIR/../extern
-
-default_institutes="akvaplan;au;bb;csiro;examples;gotm;iow;jrc;msi;nersc;niva;pclake;pml;selma;su;uhh"
-external_institutes="ersem;ogs;pisces"
-fabm_institutes="-DFABM_INSTITUTES=$default_institutes;$external_institutes"
-
+external_institutes="ogs"
 model_dirs="-DFABM_ERSEM_BASE=$external_model_dir/ersem -DFABM_OGS_BASE=$external_model_dir/ogs -DFABM_PISCES_BASE=$external_model_dir/pisces"
 
 declare -a CMAKE_PLATFORM_FLAGS
@@ -18,7 +14,7 @@ else
   CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
 fi
 
-cmake $RECIPE_DIR/.. -DPython3_EXECUTABLE="$PYTHON" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX ${CMAKE_PLATFORM_FLAGS[@]} $fabm_institutes $model_dirs
+cmake $RECIPE_DIR/.. -DPython3_EXECUTABLE="$PYTHON" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX ${CMAKE_PLATFORM_FLAGS[@]} "-DFABM_EXTRA_INSTITUTES=$external_institutes" $model_dirs
 cmake --build . --config Release --parallel $CPU_COUNT --target install
 cd ..
 rm -r build
