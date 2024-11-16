d4337359537baf9ab2afadc5b54403f1c22868a6
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2024-11-16 16:23:28 +0100

    fix compatibility with mpi4py 4 in combination with MPI<4

--- eatpy/_eat_filter_pdaf.pyx
+++ eatpy/_eat_filter_pdaf.pyx
@@ -1,6 +1,22 @@
 # cython: language_level=3
 
 cimport cython
+
+cdef extern from *:
+    """
+    #include <mpi.h>
+    
+    #if (MPI_VERSION < 3) && !defined(PyMPI_HAVE_MPI_Message)
+    typedef void *PyMPI_MPI_Message;
+    #define MPI_Message PyMPI_MPI_Message
+    #endif
+    
+    #if (MPI_VERSION < 4) && !defined(PyMPI_HAVE_MPI_Session)
+    typedef void *PyMPI_MPI_Session;
+    #define MPI_Session PyMPI_MPI_Session
+    #endif"
+    """
+
 cimport mpi4py.MPI
 cimport mpi4py.libmpi
 
